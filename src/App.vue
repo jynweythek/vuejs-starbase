ce43143c2a0ead74a9f939b95bf19697b87a11df
Rust Talapov <rust.talapov@gmail.com>
2018-04-25 12:15:56 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">
+        <h3>{{ title }}</h3>
+        <div class="row col-md-12">
+            <Character v-for="(id, index) in initialIds" :id="id" :key="index"></Character>
+        </div>
+    </div>
+</template>
+
+<script>
+  import Character from './components/Character.vue';
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        title: 'Generate your team',
+        initialIds: [1, 2, 3]
+      }
+    },
+    components: {
+      Character
+    }
+  }
+</script>
